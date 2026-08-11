8d4164022b48241599a68e9aa87580245f2235f4
annakat123 <ytivana@gmail.com>
2026-08-11 18:51:10 +0800

    Fix Citizens NPC lifecycle movement and equipment

--- src/main/java/dev/pvpbot/bot/entity/BotHandle.java
+++ src/main/java/dev/pvpbot/bot/entity/BotHandle.java
@@ -1,4 +1,4 @@
 package dev.pvpbot.bot.entity;
 import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.entity.Player;
-public record BotHandle(NPC npc, Player entity) { public boolean valid(){return npc.isSpawned()&&entity!=null&&!entity.isDead();} public void destroy(){if(npc.isSpawned())npc.despawn();npc.destroy();} }
+public record BotHandle(NPC npc, Player entity) { public boolean valid(){return entity!=null&&!entity.isDead();} public void destroy(){if(npc.isSpawned())npc.despawn();npc.destroy();} }
